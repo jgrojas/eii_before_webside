ec091d0cf2f44305b77a9d9a7f7a1821274fa9d1
jgrojas <gabriel.jugaro48@gmail.com>
2019-07-23 18:10:50 -0500

    wp-admin

---
+++ wp-admin/info.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="icon" 
+      type="image/png" 
+      href="http://static.templatetuning.com/favicon.ico">
+		<title>Server's info by Clair</title>
+	</head>
+	<body>
+    <div class="center">
+    <table cellpadding="3" border="0" width="600" height="50">
+    <tbody>
+    <tr class="h">
+    <td>
+    <h1 class="p" style="text-align:left; padding:0px 0px 0px 185px"; ><a href="http://www.templatetuning.com/" target="_blank"><img src="http://s0.uploads.im/w2Vrz.png"/></a><br>Server's info by Clair</h1>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+	<?php
+phpinfo();
+?>
+		<p><a href="http://www.templatetuning.com/"><img src="http://static.templatetuning.com/images/company_logo.png"/></a></p>
+	</body>
+</html>
